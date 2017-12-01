3b4f5218f015bb4928b5dbd20f169e9c30eb7259
AbandiGa <abandiga@gmail.com>
2017-11-30 21:50:28 -0600

    Update MenuActivity.java

--- MenuActivity.java
+++ MenuActivity.java
@@ -19,23 +19,23 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MenuActivity extends ListActivity{
-	//Our DataSource to work with database.
-		private BabyDataSource dataSource;
-		//Used to store all baby to be used in ListView adapter.
-		private List<Baby> baby = new ArrayList<Baby>();
-		//To be used as an adapter with ListView.
-		//private BabyAdapter adapter;
-	    /** Called when the activity is first created. */
-	    public void onCreate(Bundle savedInstanceState) {
-	               super.onCreate(savedInstanceState);
+    //Our DataSource to work with database.
+    private BabyDataSource dataSource;
+    //Used to store all baby to be used in ListView adapter.
+    private List<Baby> baby = new ArrayList<Baby>();
+     //To be used as an adapter with ListView.
+    //private BabyAdapter adapter;
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
 
-	        setContentView(R.layout.main);
+	setContentView(R.layout.main);
 
-	        final Button button = (Button) findViewById(R.id.);
-	        button.setOnClickListener(new View.OnClickListener() {
-	            public void onClick(View v) {
-	                // Perform action on click
-	            }
-	        });
+	final Button button = (Button) findViewById(R.id.);
+	button.setOnClickListener(new View.OnClickListener() {
+	    public void onClick(View v) {
+		// Perform action on click
 	    }
+	});
+    }
 }
